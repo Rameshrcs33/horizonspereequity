e48a3609a297c09be2734873cdd8a39fe919d66e
Rameshrcs33 <ramesh.developer.1991@gmail.com>
2025-09-12 19:46:10 +0530

    Fix: Sync package-lock.json with package.json

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,18 @@
 import { colors } from "@/constants/colors";
-import { useRouter } from "expo-router";
-import { useEffect } from "react";
+import { useFocusEffect, useRouter } from "expo-router";
 import { Image, View } from "react-native";
 
 export default function SplashScreen() {
   const router: any = useRouter();
-
-  useEffect(() => {
+  
+  useFocusEffect(() => {
     let timeout = 0;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
       goToLogin();
     }, 1500);
-  }, []);
+  });
+
 
   const goToLogin = () => {
     router.push("/(auth)");
