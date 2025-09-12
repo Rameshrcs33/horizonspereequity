58df499ebb3ec3afc247fb08ea301d021c2913f8
Rameshrcs33 <ramesh.developer.1991@gmail.com>
2025-09-12 18:30:43 +0530

    firebase config setup

---
+++ config/firebaseAppConfig.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+import firebaseConfig from "./firebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
